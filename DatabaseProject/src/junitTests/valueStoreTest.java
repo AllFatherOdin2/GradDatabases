64477f510e21840628d586291dadb824da8ab85e
John <jtbosworth@wpi.edu>
2015-02-17 15:11:58 -0500

    update classpath

---
+++ DatabaseProject/src/junitTests/valueStoreTest.java
@@ -0,0 +1,15 @@
+package junitTests;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+
+public class valueStoreTest extends TestCase {
+
+	@Test
+	public void test() {
+		valueStoreImpl valueStore = new valueStoreImpl();
+	}
+
+}
