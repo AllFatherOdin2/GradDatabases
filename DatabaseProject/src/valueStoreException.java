54c768c6c2160a722bbac599535739ab243230a7
dmodica3 <gdmodica@wpi.edu>
2015-02-17 15:00:38 -0500

    Added Javadoc to exception class constructors

--- DatabaseProject/src/valueStoreException.java
+++ DatabaseProject/src/valueStoreException.java
@@ -8,10 +8,18 @@
  *
  */
 public class valueStoreException extends Exception {
+	/**
+	 * Default constructor.
+	 */
 	public valueStoreException(){
 		super("The requested key either does not exist or could not be found in the datastore");
 	}
 	
+	/**
+	 * Passes a custom message in for the exception
+	 * 
+	 * @param msg Custom message to return in exception
+	 */
 	public valueStoreException(String msg){
 		super(msg);
 	}
