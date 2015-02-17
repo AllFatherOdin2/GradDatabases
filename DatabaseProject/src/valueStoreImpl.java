9d40ff7bfa203fbca10b345244f8bd93b2eb3201
John <jtbosworth@wpi.edu>
2015-02-17 13:47:48 -0500

    Add basic file reading

--- DatabaseProject/src/valueStoreImpl.java
+++ DatabaseProject/src/valueStoreImpl.java
@@ -1,5 +1,33 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
 
 public class valueStoreImpl {
+	public static void main(String[] args){
+		final String INPUT_FILE = "C:\\Users\\Helios\\Desktop\\test.txt";
+		
+		
+		try (InputStream inputStream = new FileInputStream(INPUT_FILE)){
+			byte[] bytes = new byte[inputStream.available()];
+			System.out.println("Available bytes from the file: "+inputStream.available());
+			
+			int bytesread = inputStream.read(bytes);
+			
+			System.out.println("Read Bytes: "+bytesread);
+			System.out.println(Arrays.toString(bytes));
+			
+		} catch(FileNotFoundException e){
+			e.printStackTrace();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		
+	}
+	
 	/**
 	 * Will read in data from a byte array, and build a text file containing the data, with the key as the name of the file
 	 * 
@@ -19,6 +47,7 @@ public class valueStoreImpl {
 	 */
 	public byte[] get(int key){
 	
+		
 		return null;
 	}
 	
