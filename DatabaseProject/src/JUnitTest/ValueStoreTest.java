5b4a34f595648e5edd09708b798e2039dfbcaa5e
John <jtbosworth@wpi.edu>
2015-02-18 14:19:36 -0500

    trying to fix gitignore

--- DatabaseProject/src/JUnitTest/ValueStoreTest.java
+++ DatabaseProject/src/JUnitTest/ValueStoreTest.java
@@ -20,7 +20,7 @@ public class ValueStoreTest extends TestCase {
 	private static ValueStoreImpl valueStore;
 	private static int intKey;
 	private static byte[] data;
-	private static final int LARGE_DATA_SIZE = 10000000;
+	private static final int LARGE_DATA_SIZE = 100000;
 	
 	@BeforeClass
     public void setUp() {
