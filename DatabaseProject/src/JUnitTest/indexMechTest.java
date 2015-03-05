049ac386b51eeabd56b809851b6b280b27383d21
John <jtbosworth@wpi.edu>
2015-03-04 19:41:37 -0500

    added Test Class

---
+++ DatabaseProject/src/JUnitTest/indexMechTest.java
@@ -0,0 +1,17 @@
+package JUnitTest;
+import java.io.File;
+import java.util.Arrays;
+
+import ProjectCode.ValueStoreException;
+import ProjectCode.ValueStoreImpl;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class indexMechTest {
+
+	
+
+}
