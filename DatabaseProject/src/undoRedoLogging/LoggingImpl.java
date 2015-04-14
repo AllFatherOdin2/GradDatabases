8a761085b0a18f2d2565807b75bd83e032b1f94d
dmodica3 <gdmodica@wpi.edu>
2015-04-14 18:20:47 -0400

    Began writing code for logging statements.

--- DatabaseProject/src/undoRedoLogging/LoggingImpl.java
+++ DatabaseProject/src/undoRedoLogging/LoggingImpl.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import ProjectCode.QueryExecException;
@@ -24,6 +25,7 @@ public class LoggingImpl {
 	private static List<List<String>> cities = new ArrayList<List<String>>();
 	private static List<List<String>> countries = new ArrayList<List<String>>();
 	private static List<String> results = new ArrayList<String>();
+	private static List<String> log = new ArrayList<String>();
 	
 	private static double queryTime = 0;
 	
@@ -35,6 +37,7 @@ public class LoggingImpl {
 	public List<String> open() throws LoggingException{
 		final String INPUT_FILE_CITY = direct + "city.csv";
 		final String INPUT_FILE_COUNTRY = direct + "country.csv";
+		final String LOG_FILE = direct + "log.txt";
 		
 		byte[] cityBytes = null;
 		try (InputStream inputStream = new FileInputStream(INPUT_FILE_CITY)){
@@ -76,20 +79,42 @@ public class LoggingImpl {
 	
 	//TODO: read one in, write to log file. "read in 'tuple id from file' population is 'old Pop' at 'Time'","writing to 'file' 'tuple id', population to 'new'", "Commit 'file'"
 	private List<String> getNext() throws LoggingException{
+		Date date = new Date();
 		for(List<String> city : cities){
-			String countryCode = city.get(2);
-			for (List<String> country : countries){
-				String code = country.get(0);
+			//String countryCode = city.get(2);
+			//Read in tuple
+			int cityPop = Integer.parseInt(city.get(4));
+			log.add("READ " + city.get(1) + " FROM city, population: " + city.get(4) + ",TIME: " + date.getTime());
+			
+			//Modify tuple
+			cityPop += .2*cityPop;
+			
+			//write out tuple
+			city.set(4, ""+cityPop);
+			log.add("WRITE " + city.get(1) + "FROM city, population: " + city.get(4) + ",TIME: " + date.getTime());
+		}
+		//write back to CSV file
+		//commit and push to log file
+		for (List<String> country : countries){
+			/*String code = country.get(0);
 				if (countryCode.compareTo(code) == 0){
 					
 					int countryPop = Integer.parseInt(country.get(6));
 					int cityPop = Integer.parseInt(city.get(4));
 					if(cityPop > .4 * countryPop){
 						results.add(city.get(1));
-					}
 				}
-			}
+			}*/
+			int countryPop = Integer.parseInt(country.get(6));
+			log.add("READ " + country.get(1) + "FROM country, population: " + country.get(6) + ",TIME: " + date.getTime());
+			
+			countryPop += .2*countryPop;
+			
+			country.set(6, ""+countryPop);
+			log.add("WRITE " + country.get(1) + "FROM country, population: " + country.get(6) + ",TIME: " + date.getTime());
 		}
+		//write to log file
+		
 		if(results.size() == 0){
 			throw new LoggingException("No matches were found.");
 		}
--- DatabaseProject/src/undoRedoLogging/README4.txt
+++ DatabaseProject/src/undoRedoLogging/README4.txt
@@ -3,3 +3,10 @@ David Modica
 CS 542 - Database Management Systems
 
 Assumptions
+-Log statement formats 
+	-"read in 'tuple id from file' population is 'old Pop' at 'Time'"
+	-"writing to 'file' 'tuple id', population to 'new'"
+	-"Commit 'file'"
+-Modification of populations only increases by 2%
+-Changes in city population do not modify country population
+
