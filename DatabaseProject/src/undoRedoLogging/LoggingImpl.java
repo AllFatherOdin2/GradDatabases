b454f65200656a9d7fe15b53e0e91993d8f6abd7
dmodica3 <gdmodica@wpi.edu>
2015-04-14 15:28:06 -0400

    Added implementation file

---
+++ DatabaseProject/src/undoRedoLogging/LoggingImpl.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package undoRedoLogging;
+
+/**
+ * @author David
+ * @author John
+ *
+ */
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LoggingImpl {
+	
+}
