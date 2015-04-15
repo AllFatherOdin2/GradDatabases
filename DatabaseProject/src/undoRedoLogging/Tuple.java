4507b7c12746044735a324aa62c2a4f210f210e8
dmodica3 <gdmodica@wpi.edu>
2015-04-15 14:15:32 -0400

    Added tuple class

---
+++ DatabaseProject/src/undoRedoLogging/Tuple.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package undoRedoLogging;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author DModica
+ * @author JBosworth
+ *
+ */
+public class Tuple {
+	List<String> data = new ArrayList<String>();
+	
+	/**
+	 * Basic Constructor
+	 * @param data Data to insert into the tuple
+	 */
+	public Tuple(List<String> data){
+		this.data = data;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public List<String> getData() {
+		return data;
+	}
+
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(List<String> data) {
+		this.data = data;
+	}
+	
+}
