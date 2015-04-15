0cee3710dd68226d23ddf363254ee31628f8adbf
John <jtbosworth@wpi.edu>
2015-04-15 14:14:48 -0400

    added relation and attribute classes

---
+++ DatabaseProject/src/undoRedoLogging/Attribute.java
@@ -0,0 +1,9 @@
+package undoRedoLogging;
+
+public class Attribute {
+	private String key;
+	
+	public Attribute(String key){
+		this.key = key;
+	}
+}
---
+++ DatabaseProject/src/undoRedoLogging/Relation.java
@@ -0,0 +1,10 @@
+package undoRedoLogging;
+
+import java.util.List;
+
+public class Relation {
+	private List<Attribute> attributes;
+	private String name;
+	private List<Tuple> tuples;
+
+}
