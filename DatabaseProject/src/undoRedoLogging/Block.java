486df0abad094f71ddde5700e593e10d90eb2852
John <jtbosworth@wpi.edu>
2015-04-17 13:18:42 -0400

    added functionality to block

--- DatabaseProject/src/undoRedoLogging/Block.java
+++ DatabaseProject/src/undoRedoLogging/Block.java
@@ -1,7 +1,20 @@
 package undoRedoLogging;
 
+import java.util.List;
+
 public class Block {
-	public Block(){
-		
+	private List<Tuple> tuples;
+	
+	public Block(List<Tuple> tuples){
+		this.tuples = tuples;
 	}
+	
+	public void addTuple(Tuple tuple){
+		tuples.add(tuple);
+	}
+	
+	public List<Tuple> getTuples(){
+		return tuples;
+	}
+	
 }
