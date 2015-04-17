d1e9eca92c784b979d0c3b621a75e4a57fa53bc5
dmodica3 <gdmodica@wpi.edu>
2015-04-17 15:24:42 -0400

    Finished getNext(), added helper for generalization, and finished close().

--- DatabaseProject/src/undoRedoLogging/LoggingImpl.java
+++ DatabaseProject/src/undoRedoLogging/LoggingImpl.java
@@ -38,7 +38,7 @@ public class LoggingImpl {
 	 * @return
 	 * @throws LoggingException
 	 */
-	public List<String> open() throws LoggingException{
+	public void open() throws LoggingException{
 		final String INPUT_FILE_CITY = direct + "city.csv";
 		final String INPUT_FILE_COUNTRY = direct + "country.csv";
 		ArrayList<String> cityAttrs = new ArrayList<String>();
@@ -72,107 +72,61 @@ public class LoggingImpl {
 		country.loadTuples(INPUT_FILE_COUNTRY);
 
 		double startTime = System.nanoTime();
-		List<String> result = getNext();
+		getNext();
 		double endTime = System.nanoTime();
 		setQueryTime((endTime-startTime)/1000000000);
-		
-		return result;
 	}
 	
 	
 	/**
-	 * 
-	 * @return
+	 * Modifies both country and city relations and calls close.
 	 * @throws LoggingException
 	 */
-	private List<String> getNext() throws LoggingException{
-		Date date = new Date();
-		//Handle city populations
-		List<Block> cityBlocks= city.getBlocks();
-		//Hardcoded because this is single purpose software at the moment. Easy enough to fix with a parameter, though.
-		int cityIndexLoc = city.getAttributeArrayLocation("Population");
-		for(Block block : cityBlocks){
-			List<Tuple> blockTuples = block.getTuples();
-			for(Tuple tuple : blockTuples){
-				int origPop = Integer.parseInt(tuple.getData().get(cityIndexLoc));
-				Log.addLog("READ, " + tuple.getData().get(0) + ", " + date.getTime());
-				int newPop = (int) (origPop * 1.02);
-				tuple.updateData(cityIndexLoc, newPop+"");
-			}
-			
-		}
-		
-		//Handle country populations
-		List<Block> countryBlocks= country.getBlocks();
-		int countryIndexLoc = country.getAttributeArrayLocation("Population");
-		for(Block block : countryBlocks){
-			List<Tuple> blockTuples = block.getTuples();
-			for(Tuple tuple : blockTuples){
-				
-			}
-		}
-		
-		
-		/*
-	 	Date date = new Date();
-		for(List<String> city : cities){
-			//String countryCode = city.get(2);
-			//Read in tuple
-			int cityPop = Integer.parseInt(city.get(4));
-			log.add("READ " + city.get(1) + " FROM city, population: " + city.get(4) + ",TIME: " + date.getTime());
-			
-			//Modify tuple
-			cityPop += .2*cityPop;
-			
-			//write out tuple
-			city.set(4, ""+cityPop);
-			log.add("WRITE " + city.get(1) + "FROM city, population: " + city.get(4) + ",TIME: " + date.getTime());
-		}
-		//write back to CSV file
-		//commit and push to log file
-		for (List<String> country : countries){
-			String code = country.get(0);
-				if (countryCode.compareTo(code) == 0){
-					
-					int countryPop = Integer.parseInt(country.get(6));
-					int cityPop = Integer.parseInt(city.get(4));
-					if(cityPop > .4 * countryPop){
-						results.add(city.get(1));
-				}
-			}
-			int countryPop = Integer.parseInt(country.get(6));
-			log.add("READ " + country.get(1) + "FROM country, population: " + country.get(6) + ",TIME: " + date.getTime());
-			
-			countryPop += .2*countryPop;
-			
-			country.set(6, ""+countryPop);
-			log.add("WRITE " + country.get(1) + "FROM country, population: " + country.get(6) + ",TIME: " + date.getTime());
-		}
-		//write to log file
-		
-		if(results.size() == 0){
-			throw new LoggingException("No matches were found.");
-		}
-	 	*/	
-		return close();
-	 
+	private void getNext() throws LoggingException{
+		modifyRelation(city);
+		modifyRelation(country);
+		close();
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Writes the log string out to a text file
 	 */
-	private List<String> close(){
+	private void close(){
 		/*
 		System.out.println(results.size());
 		for(String city : results){
 			System.out.println(city);
 		}
 		*/
-		List<String> tempResults = null;
-		
-		return tempResults;
-		
+		Log.writeToFile();
+	}
+	
+	/**
+	 * Takes a Relation with a population attribute and modifies the population of all tuples to be 102% of original value
+	 * @param table Relation being modified
+	 * @throws LoggingException
+	 */
+	private void modifyRelation(Relation table) throws LoggingException{
+		Date date = new Date();
+		//Handle city populations
+		List<Block> cityBlocks= table.getBlocks();
+		//Hardcoded because this is single purpose software at the moment. Easy enough to fix with a parameter, though.
+		int attrIndexLoc;
+		try {
+			attrIndexLoc = table.getAttributeArrayLocation("Population");
+		} catch (LoggingException e) {
+			throw new LoggingException("The population attribute does not exist for this relation");
+		}
+		for(int i = 0; i < cityBlocks.size(); i++){
+			List<Tuple> blockTuples = cityBlocks.get(i).getTuples();
+			for(Tuple tuple : blockTuples){
+				int origPop = Integer.parseInt(tuple.getData().get(attrIndexLoc));
+				Log.addLog("READ, " + tuple.getData().get(0) + ", " + date.getTime());
+				int newPop = (int) (origPop * 1.02);
+				tuple.updateData(attrIndexLoc, newPop+"");
+			}
+			table.updateBlock(i, cityBlocks.get(i));
+		}
 	}
 	
 	public double getQueryTime() {
